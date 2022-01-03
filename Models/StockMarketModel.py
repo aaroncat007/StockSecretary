e3c47066b76972411c94b6b1c13115d75a1ad356
Aaron.Fan <aaron.fan@mterrace.com.tw>
2022-01-03 17:17:01 +0800

    加入大盤模型

---
+++ Models/StockMarketModel.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+from Repository.SQLiteRepository import SQLiteRepository
+from Models.dbModel import dbModel
+import datetime
+import yahoo_fin.stock_info as si
+import yfinance as yf
+
+class StockMarketModel(dbModel):
+    "大盤指數模型"
+
+    TABLENAME = 'stockMarket'
+
+    def __init__(self):
+        # 取得資料倉儲
+        self.repo = SQLiteRepository()
+        self.initTable()
+
+    #取得台股大盤資訊
+    def getTWStock(self):
+        id='^TWII'
+        responseStr = ""
+        toclos=float(yf.download(id,
+                    period='1d',
+                    interval='5m').iloc[-1:]['Close'])
+        yes=si.get_data(id).iloc[-1:]
+        yeclos=float(yes['close'])
+        yesop=float(yes['open']) 
+        yeshg=float(yes['high']) 
+        yeslo=float(yes['low']) 
+        yesvalue=int(yes['volume']) 
+        responseStr +=(f'大盤情形 \n')
+        if toclos>yeclos:
+            responseStr +=(f'現價{round(toclos,2)} ▲ {toclos-yeclos}, {round((toclos/yeclos-1)*100,2)}% \n')
+            responseStr +=(f'昨日開盤    {round(yesop,2)} \n')
+            responseStr +=(f'昨日收盤    {round(yeclos,2)} \n')
+            responseStr +=(f'昨日最高    {round(yeshg,2)} \n')
+            responseStr +=(f'昨日最低    {round(yeslo,2)} \n')
+            responseStr +=(f'昨日成交量    {yesvalue} 張 \n')
+
+        elif toclos==yeclos:
+            responseStr +=(f'現價{round(toclos,2)} ● {toclos-yeclos}, {round((toclos/yeclos-1)*100,2)}% \n')
+            responseStr +=(f'昨日開盤    {round(yesop,2)} \n')
+            responseStr +=(f'昨日收盤    {round(yeclos,2)} \n')
+            responseStr +=(f'昨日最高    {round(yeshg,2)} \n')
+            responseStr +=(f'昨日最低    {round(yeslo,2)} \n')
+            responseStr +=(f'昨日成交量    {yesvalue} 張 \n')
+
+        else:
+            responseStr +=(f'{toclos} ▼ {yeclos-toclos}, {round((yeclos/toclos-1)*100,2)}% \n')
+            responseStr +=(f'昨日開盤    {round(yesop,2)} \n')
+            responseStr +=(f'昨日收盤    {round(yeclos,2)} \n')
+            responseStr +=(f'昨日最高    {round(yeshg,2)} \n')
+            responseStr +=(f'昨日最低    {round(yeslo,2)} \n')
+            responseStr +=(f'昨日成交量  {yesvalue} 張 \n')
+
+        return responseStr
+
+
+    def initTable(self):
+        """
+        初始化資料表
+        """
+        conn = self.repo.getConn()
+        c = conn.cursor()
+        pass
--- Services/TWStockHandler.py
+++ Services/TWStockHandler.py
@@ -2,12 +2,16 @@
 # -*- coding:utf-8 -*-
 
 from Services.MessageHandler import MessageHandler
+from Models.StockMarketModel import StockMarketModel
 
 # 大盤指數
 class TWStockHandler(MessageHandler):
 
     def __init__(self):
         super().__init__()
+        self.smModel = StockMarketModel()
 
     def getMessage(self):
-        return super().db.getTWStock()
+        "取得回應訊息"
+        response = self.smModel.getTWStock()
+        return response
